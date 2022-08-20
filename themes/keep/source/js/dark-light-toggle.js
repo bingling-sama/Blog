a2a904f535eff89185d92f8ba199585be07cc6ce
bingling_sama <bingling_sama@qq.com>
2022-08-20 18:01:36 +0800

    format .js files

--- themes/keep/source/js/dark-light-toggle.js
+++ themes/keep/source/js/dark-light-toggle.js
@@ -1,58 +1,56 @@
-/* global KEEP */
-
 KEEP.initModeToggle = () => {
 
-  KEEP.utils.modeToggle = {
+	KEEP.utils.modeToggle = {
 
-    modeToggleButton_dom: document.querySelector('.tool-dark-light-toggle'),
-    iconDom: document.querySelector('.tool-dark-light-toggle i'),
+		modeToggleButton_dom: document.querySelector('.tool-dark-light-toggle'),
+		iconDom: document.querySelector('.tool-dark-light-toggle i'),
 
-    enableLightMode() {
-      document.body.classList.remove('dark-mode');
-      document.body.classList.add('light-mode');
-      this.iconDom.className = 'fas fa-moon';
-      KEEP.styleStatus.isDark = false;
-      KEEP.setStyleStatus();
-    },
+		enableLightMode() {
+			document.body.classList.remove('dark-mode')
+			document.body.classList.add('light-mode')
+			this.iconDom.className = 'fas fa-moon'
+			KEEP.styleStatus.isDark = false
+			KEEP.setStyleStatus()
+		},
 
-    enableDarkMode() {
-      document.body.classList.add('dark-mode');
-      document.body.classList.remove('light-mode');
-      this.iconDom.className = 'fas fa-sun';
-      KEEP.styleStatus.isDark = true;
-      KEEP.setStyleStatus();
-    },
+		enableDarkMode() {
+			document.body.classList.add('dark-mode')
+			document.body.classList.remove('light-mode')
+			this.iconDom.className = 'fas fa-sun'
+			KEEP.styleStatus.isDark = true
+			KEEP.setStyleStatus()
+		},
 
-    isDarkPrefersColorScheme() {
-      return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
-    },
+		isDarkPrefersColorScheme() {
+			return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
+		},
 
-    initModeStatus() {
-      const styleStatus = KEEP.getStyleStatus();
+		initModeStatus() {
+			const styleStatus = KEEP.getStyleStatus()
 
-      if (styleStatus) {
-        styleStatus.isDark ? this.enableDarkMode() : this.enableLightMode();
-      } else {
-        this.isDarkPrefersColorScheme().matches ? this.enableDarkMode() : this.enableLightMode();
-      }
-    },
+			if (styleStatus) {
+				styleStatus.isDark ? this.enableDarkMode() : this.enableLightMode()
+			} else {
+				this.isDarkPrefersColorScheme().matches ? this.enableDarkMode() : this.enableLightMode()
+			}
+		},
 
-    initModeToggleButton() {
-      this.modeToggleButton_dom.addEventListener('click', () => {
-        const isDark = document.body.classList.contains('dark-mode');
-        isDark ? this.enableLightMode() : this.enableDarkMode();
-      });
-    },
+		initModeToggleButton() {
+			this.modeToggleButton_dom.addEventListener('click', () => {
+				const isDark = document.body.classList.contains('dark-mode')
+				isDark ? this.enableLightMode() : this.enableDarkMode()
+			})
+		},
 
-    initModeAutoTrigger() {
-      const isDarkMode = this.isDarkPrefersColorScheme();
-      isDarkMode.addEventListener('change', e => {
-        e.matches ? this.enableDarkMode() : this.enableLightMode();
-      });
-    }
-  }
+		initModeAutoTrigger() {
+			const isDarkMode = this.isDarkPrefersColorScheme()
+			isDarkMode.addEventListener('change', e => {
+				e.matches ? this.enableDarkMode() : this.enableLightMode()
+			})
+		}
+	}
 
-  KEEP.utils.modeToggle.initModeStatus();
-  KEEP.utils.modeToggle.initModeToggleButton();
-  KEEP.utils.modeToggle.initModeAutoTrigger();
-};
+	KEEP.utils.modeToggle.initModeStatus()
+	KEEP.utils.modeToggle.initModeToggleButton()
+	KEEP.utils.modeToggle.initModeAutoTrigger()
+}
