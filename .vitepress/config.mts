f2df1dcb733c4831a519aad5807460ca0fa032c7
bingling-sama <bingling_sama@qq.com>
2023-09-18 23:38:52 +0800

    ✨ feat(formatter): eslint & prettier
    config eslint & prettier

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Booling",
-  description: "bingling_sama's personal blog site"
+	title: "Booling",
+	description: "bingling_sama's personal blog site"
 })
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
-import Layout from './Layout.vue'
-import './style.css'
+import Layout from "./Layout.vue"
+import "./style.css"
 
 export default {
-  Layout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+	Layout,
+	enhanceApp({ app, router, siteData }) {
+		// ...
+	}
 }
-
