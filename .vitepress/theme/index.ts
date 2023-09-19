983ad425f0b51a2afb17a49f0ffa41921e9fa220
bingling-sama <bingling_sama@qq.com>
2023-09-20 00:21:31 +0800

    ✨ feat(vuetify): add vuetify

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,21 @@
 // https://vitepress.dev/guide/custom-theme
 import Layout from "./Layout.vue"
+import { createVuetify } from "vuetify"
+import { EnhanceAppContext } from "vitepress"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 import "./style.css"
+import "vuetify/styles"
+
+const vuetify = createVuetify({
+	components,
+	directives
+})
 
 export default {
 	Layout,
-	enhanceApp({ app, router, siteData }) {
+	enhanceApp(ctx: EnhanceAppContext) {
 		// ...
+		ctx.app.use(vuetify)
 	}
 }
