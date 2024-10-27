2c3a9d3ea806ab0cc3f3706c605c341805b60c2b
bingling-sama <bingling_sama@qq.com>
2024-10-27 12:59:21 +0800

    ✨ feat: new theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,16 @@
 // https://vitepress.dev/guide/custom-theme
-import Layout from './Layout.vue'
-import type { Theme } from 'vitepress'
-import './style.css'
-import "./assets/fonts/zpix.ttf"
+// import Layout from './Layout.vue'
+// import type { Theme } from 'vitepress'
+// import './style.css'
+// import "./assets/fonts/zpix.ttf"
 
-export default {
-  Layout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-	}
-} satisfies Theme
+// export default {
+//   Layout,
+//   enhanceApp({ app, router, siteData }) {
+//     // ...
+// 	}
+// } satisfies Theme
 
+import Theme from "../../theme"
+
+export default Theme
