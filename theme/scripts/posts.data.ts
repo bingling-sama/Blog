5a65d5e48c4dd01a6ed42f21ade3b80434fd7b6c
bingling-sama <bingling_sama@qq.com>
2024-11-06 21:15:31 +0800

    ✨ feat: transform post date format

--- theme/scripts/posts.data.ts
+++ theme/scripts/posts.data.ts
@@ -7,9 +7,14 @@ export default createContentLoader("posts/**/*.md", {
 			url: url,
 			title: frontmatter.title,
 			description: frontmatter.description,
-			date: frontmatter.date,
+			date: transformDate(frontmatter.date),
 			category: frontmatter.category,
 			tags: frontmatter.tags
 		} as Post))
 	}
 })
+
+function transformDate(date: string): string {
+	const d = new Date(date);
+	return d.toISOString().replace('T', ' ').substring(0, 19);
+}
